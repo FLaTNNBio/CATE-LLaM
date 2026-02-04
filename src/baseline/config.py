8947125ceb042bf277964c0c6947ca20b4392696
Type-Here <94553428+Type-Here@users.noreply.github.com>
2026-02-04 22:24:22 +0100

    added RBC_V2 and SEPSIS_V2 configurations: include new baseline configs for dataset selection and updated drop columns

--- src/baseline/config.py
+++ src/baseline/config.py
@@ -55,13 +55,31 @@ RBC_V1_FIXED = BaselineConfig(
     outcome_col="y_hosp_mort",
     # Empty for now
     drop_cols=["elig_hb_threshold","max_elig_hb_threshold",
-               "min_elig_hb_threshold", "treat_window_hours",
+               "min_elig_hb_threshold", "treat_window_hours", "y_24h_mort", "y_hosp_mort",
                "elig_within_hours", "y_hosp_mort", "t_rbc_3h", "hadm_id", "intime"], # "t0_hb", "hemoglobin", "has_lactate", "bicarbonate", "potassium"],
     # Same weighting settings initially used in RBC analyses
     ps_clip=(0.01, 0.99),
     weight_trim_quantiles=(0.01, 0.99),
     out_dir="artifacts/cate/rbc_v1_fixed"
 )
+
+
+RBC_V2 = BaselineConfig(
+    data_path="data/analytic/analytic_sepsis_steroids_mit_v2.parquet",
+    id_col="stay_id",
+    subject_col="subject_id",
+    treatment_col="t_rbc_3h",
+    outcome_col="y_hosp_mort",
+    # Empty for now
+    drop_cols= [ "treat_window_hours", "y_24h_mort", "y_hosp_mort", "Fi02_bg", "lactate", "BMI",
+                 "stay_id", "hadm_id", "intime", "rbc_units_proxy", "po2", "has_lactate", "chloride",
+                 "elig_within_hours", "y_hosp_mort", "t_rbc_3h",], # "t0_hb", "hemoglobin", "has_lactate", "bicarbonate", "potassium"],
+    # Same weighting settings initially used in RBC analyses
+    ps_clip=(0.05, 0.95),
+    weight_trim_quantiles=(0.05, 0.95),
+    out_dir="artifacts/cate/rbc_v2"
+)
+
 
 RESP_V1 = BaselineConfig(
     data_path="data/analytic/analytic_resp_v1_clean.parquet",
@@ -93,4 +111,40 @@ CRRT_V1 = BaselineConfig(
     tau_direction="lte",
     policy="tau_lt_0",
     n_folds=2
+)
+
+SEPSIS_V1 = BaselineConfig(
+    data_path="data/analytic/analytic_sepsis_steroids_clean_v1.parquet",
+    id_col="stay_id",
+    subject_col="subject_id",
+    treatment_col="treat_steroid",
+    outcome_col="y_28d_mort_inhosp",
+    drop_cols=["intime", "t0_time","t0_support", "y_hosp_mort", # "y_28d_mort_inhosp"
+               "enroll_end", "treat_steroid", "steroid_unparsed_events",
+               "steroid_total_events", "hc_equiv_mg_0_24h", "lactate",
+               "stay_id", "subject_id", "hadm_id"], # "glucose"
+    ps_clip=(0.01, 0.99),
+    weight_trim_quantiles=(0.01, 0.99),
+    out_dir="artifacts/cate/sepsis_v1",
+    tau_direction="lte",
+    policy="tau_lt_0",
+    n_folds=5
+)
+
+SEPSIS_V2 = BaselineConfig(
+    data_path="data/analytic/analytic_sepsis_steroids_mit_v2.parquet",
+    id_col="stay_id",
+    subject_col="subject_id",
+    treatment_col="treat_steroid",
+    outcome_col="y_28d_mort_inhosp",
+    drop_cols=["intime", "t0_time","t0_support", "y_hosp_mort", "SO2_bg",# "y_28d_mort_inhosp"
+               "enroll_end", "treat_steroid", "steroid_unparsed_events",
+               "steroid_total_events", "hc_equiv_mg_0_24h", "lactate",
+               "stay_id", "subject_id", "hadm_id"], # "glucose"
+    ps_clip=(0.01, 0.99),
+    weight_trim_quantiles=(0.01, 0.99),
+    out_dir="artifacts/cate/sepsis_v2",
+    tau_direction="lte",
+    policy="tau_lt_0",
+    n_folds=5
 )
