69c63ae917c5db504b9a95f7023e46a25846a5fe
fedeg <fedegraz03@gmail.com>
2026-03-22 21:39:01 +0100

    feat: update configuration to read data from input and reduce tokens in bio prompt
    - Modified configuration to accept data from user input

--- src/BioMistral/calculated_cate.py
+++ src/BioMistral/calculated_cate.py
@@ -1,3 +1,15 @@
+"""
+python -m src.BioMistral.calculated_cate `
+--provider openrouter `
+--model_id mistralai/mistral-7b-instruct-v0.1 `
+--dataset_path data/aids/aids_csv.csv `
+--prompt_path src/BioMistral/prompts/short_prompt.txt `
+--output_path result_cate_llm_BioMistra/\ `
+--chunk_size 30 `
+--max_retries 0
+"""
+
+import argparse
 from pathlib import Path
 
 from src.BioMistral.llm_hf.llm_factory import LLMFactory
@@ -12,27 +24,44 @@ from src.BioMistral.domain.cate_estimator import CATEEstimator
 from src.BioMistral.pipeline.cate_pipeline import CATEPipeline
 
 
+def parse_args():
+    parser = argparse.ArgumentParser(description="Calculate cate")
+
+    #LLM config
+    parser.add_argument("--provider", type=str,required=True, default="openrouter", help="LLM provider")
+    parser.add_argument("--model_id", type=str,required=True, default="google/gemma-3-27b-it" ,help="model id")
+
+
+    #Pipeline config
+    parser.add_argument("--dataset_path", type=str,required=True, default="data/aids/aids_csv.csv", help="dataset path")
+    parser.add_argument("--prompt_path", type=str,required=True, default="src/BioMistral/prompts/bio_prompt.txt", help="prompt path")
+    parser.add_argument("--output_path", type=Path,required=True, help="output path")
+    parser.add_argument("--chunk_size", type=int,required=False, default=8, help="chunk size")
+    parser.add_argument("--max_retries", type=int,required=False, default=3, help="max retries")
+    return parser.parse_args()
+
+
 def main():
     setup_logging()
+    args = parse_args()
+    #models
+    #      model_id="meta-llama/Meta-Llama-3-8B-Instruct"
+    #      model_id = "mistralai/mistral-7b-instruct-v0.1"
+    #      model_id="google/gemma-3-27b-it"
 
     # configuration
     config = AppConfig(
-        # llm=LLMConfig(
-        #     provider="hf",
-        #     model_id="meta-llama/Meta-Llama-3-8B-Instruct"
-        # ),
-
         llm=LLMConfig(
-             provider="openrouter",
-             model_id="google/gemma-3-27b-it"
+            provider=args.provider,
+            model_id=args.model_id,
         ),
 
         pipeline=PipelineConfig(
-            dataset_path=Path("data/analytic/analytic_sepsis_early_diuretics_v1.parquet"),
-            prompt_path=Path("src/BioMistral/prompts/prompt.txt"),
-            output_path=Path("result_cate_llm/calculated_cate3.csv"),
-            chunk_size=5,
-            max_retries=3
+            dataset_path=args.dataset_path,
+            prompt_path=args.prompt_path,
+            output_path=args.output_path,
+            chunk_size=args.chunk_size,
+            max_retries=args.max_retries
         )
     )
 
@@ -53,7 +82,7 @@ def main():
 
     # result saver
     saver = ResultSaver(
-        output_dir=config.pipeline.output_path.parent,
+        output_dir=config.pipeline.output_path,
         model_name=config.llm.model_id
     )
 
